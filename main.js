ca6e3000f40be1f416c13ffde053555a8c5e457a
Arabian <shlassou@live.fr>
2021-12-26 17:51:03 +0100

    add nav bar

---
+++ main.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.link')
+
+const button = document.querySelector('.navbar-toggler')
+
+
+
+button.addEventListener('click', ()=>{
+
+  nav.classList.toggle('collapse')
+} 
+
+)
